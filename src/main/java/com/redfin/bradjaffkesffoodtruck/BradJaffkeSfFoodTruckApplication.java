abef4ced5688e9d3eb171532157e667da0aee0c6
Brad Jaffke <bradjaffke@brads-mbp.lan>
2021-03-15 20:06:18 -0700

    Initial commit

---
+++ src/main/java/com/redfin/bradjaffkesffoodtruck/BradJaffkeSfFoodTruckApplication.java
@@ -0,0 +1,13 @@
+package com.redfin.bradjaffkesffoodtruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradJaffkeSfFoodTruckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradJaffkeSfFoodTruckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redfin/bradjaffkesffoodtruck/BradJaffkeSfFoodTruckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redfin.bradjaffkesffoodtruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradJaffkeSfFoodTruckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
